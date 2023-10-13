98d9224381798b6aead0a129a69a98907959b4ff
sanjayaravinth <sanjayaravinth26@gmail.com>
2023-10-13 13:04:55 +0530

    Restful API for CRUD Operation

--- project_1/src/main/java/com/sanjay/Main.java
+++ project_1/src/main/java/com/sanjay/Main.java
@@ -2,14 +2,57 @@ package com.sanjay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){}
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId")  Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer( @PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request ){
+        Customer customer = customerRepository.getReferenceById(id);
+        customer.setName(request.name()==null ? customer.getName() : request.name() );
+        customer.setAge(request.age() == null ? customer.getAge() : request.age());
+        customer.setEmail(request.email()==null ? customer.getEmail() : request.email());
+        customerRepository.save(customer);
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+
+        customerRepository.save(customer);
     }
 
     @GetMapping("/greet")
